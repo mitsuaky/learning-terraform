3992f550844fb55c916541bb1668a2a8600e8083
Pármenas Haniel <parmenashp@gmail.com>
2022-09-14 12:26:00 -0300

    Add 01

---
+++ 01- Primeiro Script/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "1.2.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "mit-dev-terraform-learning-omg"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test-bucket-acl" {
+  bucket = aws_s3_bucket.test-bucket.id
+  acl    = "private"
+
+}
